6875f95cec896fd992d0123f579c615f2650b3b7
Mustafa Can Yucel <m.can.yucel.1@gmail.com>
2025-02-25 16:41:34 +0300

    wip

--- app/src/main/java/com/mustafacanyucel/fireflyiiishortcuts/services/dialog/DialogService.kt
+++ app/src/main/java/com/mustafacanyucel/fireflyiiishortcuts/services/dialog/DialogService.kt
@@ -1,6 +1,7 @@
 package com.mustafacanyucel.fireflyiiishortcuts.services.dialog
 
 import android.app.Activity
+import android.graphics.Color
 import android.view.View
 import com.google.android.material.snackbar.Snackbar
 import javax.inject.Inject
@@ -11,7 +12,11 @@ class DialogService @Inject constructor(
     override fun showErrorSnackbar(message: String, action: (() -> Unit)?, actionTitle: String?) {
         val rootView = activity.findViewById<View>(android.R.id.content)
         val snackbar = Snackbar.make(rootView, message, Snackbar.LENGTH_LONG)
+            .apply {
+                setBackgroundTint(Color.RED)
+            }
         if (action != null) {
+            snackbar.setActionTextColor(Color.WHITE)
             snackbar.setAction(actionTitle ?: "Action") {
                 action.invoke()
             }
